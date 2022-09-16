63eaba880df1aa0ffb4f60a8a8e9a3f18ff881b8
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-09-16 18:38:25 -0300

    Adiciona componente do header do site
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

---
+++ src/components/HeaderSemana.vue
@@ -0,0 +1,17 @@
+<template>
+    <LogoHorizontal/>
+</template>
+
+<script>
+import LogoHorizontal from './LogoHorizontal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LogoHorizontal
+  }
+}
+</script>
+
+<style scoped>
+</style>
