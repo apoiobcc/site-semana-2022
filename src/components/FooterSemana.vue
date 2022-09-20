293d43db53f8a71adf6806ed256a05a03baa60af
mariliatd <mariliatd@gmail.com>
2022-09-19 22:54:40 -0300

    Adiciona componente do footer

---
+++ src/components/FooterSemana.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-footer>
+        <v-row align="center" justify="center">
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logoime.svg"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+            <v-col cols="5" md="1" lg="3" offset-lg="0">
+                <v-img src="@/assets/images/logobcc.png"></v-img>
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<style scoped>
+footer {
+  margin: 0;
+  max-height: 20em;
+  background-color: #ff5757;
+}
+</style>
+
