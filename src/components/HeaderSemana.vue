868ebf736afe4f560d69e21ff0ed21483c2a7627
xnths <jonathascastilhoes@gmail.com>
2022-09-19 21:53:55 -0300

    Adiciona menu e descrição
    Signed-off-by: xnths <jonathascastilhoes@gmail.com>

--- src/components/HeaderSemana.vue
+++ src/components/HeaderSemana.vue
@@ -1,5 +1,22 @@
 <template>
-    <LogoHorizontal/>
+  <v-container class="header">
+    <v-row class="header-top">
+      <LogoHorizontal/>
+      <v-tabs class="header-tab"
+        background-color="white"
+        center-active
+      >
+        <a href="#sobre"><v-tab>Sobre</v-tab></a>
+        <a href=""><v-tab>Calendário</v-tab></a>
+        <a href=""><v-tab>Anos anteriores</v-tab></a>
+        <a href=""><v-tab>Como chegar</v-tab></a>
+        <a href=""><v-tab>Colaboradores</v-tab></a>
+      </v-tabs>
+    </v-row>
+    <v-row no-gutters class="descricao">
+      Semana de palestras que abordam temas variados sobre as diversas áreas da computação. O evento é aberto e qualquer interessado pode assistir as palestras.
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -14,4 +31,29 @@ export default {
 </script>
 
 <style scoped>
+  .header-top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+  .header-tab {
+    color: #fff;
+  }
+  .header-nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+	}
+  .descricao {
+    width: 500px;
+    margin: auto;
+    padding: 0.5rem;
+    text-align: center;
+    font-family: 'Merriweather';
+    font-size: 1rem;
+}
 </style>
