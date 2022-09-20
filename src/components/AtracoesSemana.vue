1b21647d6f4e34e67526dfe738c2caeef8c98452
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-09-19 23:26:49 -0300

    Adiciona a seção de atrações da semana da computação
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- src/components/AtracoesSemana.vue
+++ src/components/AtracoesSemana.vue
@@ -1,66 +1,93 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-card-text>
-      <div>Word of the Day</div>
-      <p class="text-h4 text--primary">
-        el·ee·mos·y·nar·y
-      </p>
-      <p>adjective</p>
-      <div class="text--primary">
-        relating to or dependent on charity; charitable.<br>
-        "an eleemosynary educational institution."
+  <v-row class="bar">
+    <template
+      v-for="(item, index) in cards"
+      :key="index">
+      <div
+        class="card"
+      >
+        <div class="icon">
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+        </div>
+        <h3>
+          {{ item.title }}
+        </h3>
+        <p class="desc">{{ item.desc }}</p>  
       </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        color="teal accent-4"
-        @click="reveal = true"
-      >
-        Learn More
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <v-card
-        v-if="reveal"
-        class="transition-fast-in-fast-out v-card--reveal"
-        style="height: 100%;"
-      >
-        <v-card-text class="pb-0">
-          <p class="text-h4 text--primary">
-            Origin
-          </p>
-          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-        </v-card-text>
-        <v-card-actions class="pt-0">
-          <v-btn
-            text
-            color="teal accent-4"
-            @click="reveal = false"
-          >
-            Close
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-expand-transition>
-  </v-card>
+      <div v-if="index < cards.length - 1" class="vert-divider"></div>
+    </template>
+  </v-row>
 </template>
 
 <script>
   export default {
     data: () => ({
-      reveal: false,
+      cards: [
+        {
+          icon: "mdi-coffee",
+          title: "COFFEE BREAKS",
+          desc: "A cada intervalo dos dias da semana serão oferecidos coffee breaks aos participantes!"
+        },
+        {
+          icon: "mdi-account",
+          title: "PALESTRAS",
+          desc: "Diversas empresas, grupos de pesquisa e nomes renomados da computação promoverão palestras."
+        },
+        {
+          icon: "mdi-forum",
+          title: "INTEGRAÇÃO",
+          desc: "Entre em contato com empresas e pesquisadores dos mais variados ramos da computação."
+        },
+      ],
     }),
   }
 </script>
 
 <style scoped>
+.bar {
+  background-color: #272727;
+  padding: 30px 40px;
+  font-family: Merriweather Sans;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 230px;
+  margin: 10px 30px;
+}
+
+.icon {
+  font-size: 28px;
+  text-align: center;
+  color: #fff;
+}
+
+h3 {
+  font-weight: bold;
+  text-align: center;
+  padding: 15px 0px;
+  color: #fff;
+}
+
+.desc {
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
+}
+
+.vert-divider {
+  height: 120px;
+  width: 1px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(255, 255, 255, 1), rgba(0, 0, 0, 0));
+}
+
 .v-card--reveal {
-  bottom: 0;
+    bottom: 0;
   opacity: 1 !important;
   position: absolute;
   width: 100%;
