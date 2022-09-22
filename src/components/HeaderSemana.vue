ca4f6c5d433dc5ae8b7d38ac938b091424ebc4f4
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-09-22 15:43:01 -0300

    Atualiza o header com informações de data e com a figura do grafo
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- src/components/HeaderSemana.vue
+++ src/components/HeaderSemana.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="grafo2"><img :src="require(`../assets/grafo.svg`)" height="600" /></div>
   <v-container class="header">
     <v-row class="header-top">
       <LogoHorizontal/>
@@ -9,11 +10,17 @@
         <a href="#sobre"><v-tab>Sobre</v-tab></a>
         <a href="#calendario"><v-tab>Calendário</v-tab></a>
         <a href="#como-chegar"><v-tab>Como chegar</v-tab></a>
-        <a href="#Colaboradores"><v-tab>Colaboradores</v-tab></a>
+        <a href="#colaboradores"><v-tab>Colaboradores</v-tab></a>
       </v-tabs>
     </v-row>
-    <v-row no-gutters class="descricao">
-      Semana de palestras que abordam temas variados sobre as diversas áreas da computação. O evento é aberto e qualquer interessado pode assistir as palestras.
+    <v-row no-gutters class="about">
+      <p class="descricao" id="sobre">
+        A semana da computação é uma semana de palestras que aborda diversos temas relacionados às áreas da computação. O evento é aberto a todo público e qualquer interessado pode assistir às palestras.
+      </p>
+
+      <p class="days">
+        <span class="top">de <span class="number">19</span> a <span class="number">23</span></span> <br>de outubro de 2022
+      </p>
     </v-row>
   </v-container>
 </template>
@@ -33,6 +40,7 @@ export default {
   .header-top {
     display: flex;
     flex-direction: row;
+    margin: 0px;
     justify-content: space-between;
   }
   a {
@@ -47,11 +55,39 @@ export default {
     flex-direction: row;
     justify-content: center;
 	}
+  .about {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 550px;
+  }
   .descricao {
-    width: 600px;
-    text-align: justify;
+    width: 500px;
     color: #fff;
-    font-family: 'Merriweather';
-    font-size: 1rem;
+    font-family: 'Merriweather Sans';
+    font-size: 1em;
+    margin: 50px 0px;
 }
+  .grafo2 { 
+    width: 400px;
+    object-fit: cover;
+    position: absolute;
+    top: 100px;
+    right: 0;
+    opacity: 70%;
+    overflow: hidden;
+  }
+  .days {
+    font-family: 'Merriweather Sans';
+    color: #fffd4c;
+    line-height: 1.2;
+    font-size: 22px;
+    margin: 20px 0px;
+  }
+  .days .top {
+    font-size: 36px;
+  }
+  .days .top .number {
+    font-size: 64px;
+  }
 </style>
