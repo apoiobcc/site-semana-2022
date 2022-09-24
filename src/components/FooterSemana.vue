1d1e8be0f747f55b5c8c916077bd94f18c0cf4b2
mariliatd <mariliatd@gmail.com>
2022-09-24 20:25:29 -0300

    adiciona logo poatek
    Aguardando logos dos demais patrocinadores.
    
    Signed-off-by: mariliatd <mariliatd@gmail.com>

--- src/components/FooterSemana.vue
+++ src/components/FooterSemana.vue
@@ -1,39 +1,50 @@
 <template>
     <v-footer>
-        <v-row align="center" justify="center">
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logoime.svg"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-            <v-col cols="5" md="1" lg="3" offset-lg="0">
-                <v-img src="@/assets/images/logobcc.png"></v-img>
-            </v-col>
-        </v-row>
+        <v-col cols="4">
+            <v-row align="center" justify="center">
+                <v-col cols="10" lg="8" offset-lg="0">
+                    <v-img src="@/assets/images/logoime.svg"></v-img>
+                </v-col>
+                <v-col cols="10" lg="8" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+            </v-row>
+        </v-col>
+
+        <v-col cols="8">
+            <v-row align="center" justify="center">
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logopoatek.png"></v-img>
+                </v-col>
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logobcc.png"></v-img>
+                </v-col>
+            </v-row>
+        </v-col>
     </v-footer>
 </template>
 
 <style scoped>
 footer {
   margin: 0;
-  max-height: 20em;
+  max-height: 25em;
   background-color: #ff5757;
+}
+
+p {
+    font-size: 16pt;
 }
 </style>
 
