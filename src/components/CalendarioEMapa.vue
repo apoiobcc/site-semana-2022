8635e6b06deebcd7a957f0fb6d0d4c3fcf93ff9a
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-09-27 23:14:53 -0300

    Adiciona seção que integra o calendário e o mapa do local
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

---
+++ src/components/CalendarioEMapa.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex flex-column">
+  <div class="calendar-sec">
+    <CalendarioSemana />
+  </div>
+  <div class="map-sec">
+    <MapaSemana />
+  </div>
+  </div>
+</template>
+
+<script>
+import CalendarioSemana from './CalendarioSemana.vue'
+import MapaSemana from './MapaSemana.vue'
+
+export default {
+  name: 'CalendarioESemana',
+
+  components: {
+    CalendarioSemana,
+    MapaSemana,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style scoped>
+.calendar-sec {
+  flex: 1;
+}
+.map-sec {
+  flex: 1;
+}
+</style>
