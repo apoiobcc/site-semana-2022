cf4c9a843ea558489cdd2a4ce77b098aac7b2f6c
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-09-28 23:12:27 -0300

    Modifica footer com fundo preto e adiciona colaboradores
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- src/components/FooterSemana.vue
+++ src/components/FooterSemana.vue
@@ -3,7 +3,7 @@
         <v-col cols="4">
             <v-row align="center" justify="center">
                 <v-col cols="10" lg="8" offset-lg="0">
-                    <v-img src="@/assets/images/logoime.svg"></v-img>
+                    <v-img class="ime-logo" src="@/assets/images/logoime.svg"></v-img>
                 </v-col>
                 <v-col cols="10" lg="8" offset-lg="0">
                     <v-img src="@/assets/images/logobcc.png"></v-img>
@@ -12,10 +12,19 @@
         </v-col>
 
         <v-col cols="8">
+            <v-row justify="center"><h2>Colaboradores</h2></v-row>
             <v-row align="center" justify="center">
                 <v-col cols="6" lg="4" offset-lg="0">
                     <v-img src="@/assets/images/logopoatek.png"></v-img>
-                </v-col>            
+                </v-col>        
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logoopus.png"></v-img>
+                </v-col> 
+            </v-row>
+            <v-row align="center" justify="center">
+                <v-col cols="6" lg="4" offset-lg="0">
+                    <v-img src="@/assets/images/logotagimf.svg"></v-img>
+                </v-col>  
             </v-row>
         </v-col>
     </v-footer>
@@ -25,7 +34,18 @@
 footer {
   margin: 0;
   max-height: 25em;
-  background-color: #ff5757;
+  background-color: #272727;
+}
+
+h2 {
+  color: #fff;
+  font-family: 'Merriweather Sans';
+  margin: 20px 0px;
+}
+
+.ime-logo {
+  -webkit-filter: invert(100%); /* safari 6.0 - 9.0 */
+          filter: invert(100%);
 }
 </style>
 
